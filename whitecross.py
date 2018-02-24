4e44c780f8b2b2a081fd23b5407f8e5bcee24c2a
ashlarin <33299461+ashlarin@users.noreply.github.com>
2018-02-24 14:47:01 -0800

    Update whitecross.py
    adds orientation

--- whitecross.py
+++ whitecross.py
@@ -1,12 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
-
-
-
 def bluepiece( blue[3][3], orange[3][3], green[3][3], red[3][3], yellow[3][3], white[3][3] ):
     #if piece is on the bottom layer
     for i in 3:
@@ -84,7 +75,7 @@ def orangepiece( blue[3][3], orange[3][3], green[3][3], red[3][3], yellow[3][3],
             for j in 3:
                     #turn B once, turn Ri once
                     return true
-        if red[3][i] == "o" and yellow[3][i] == "w":
+        if red[3][i] == "o" and yellow[3][i] == "w":`
             #turn Di once, turn R twice
     
     #if piece is on the second layer
@@ -109,7 +100,7 @@ def orangepiece( blue[3][3], orange[3][3], green[3][3], red[3][3], yellow[3][3],
         #turn Ri once, U once
         return true
     
-    if green[2][3] == "o" and red[2][1] == "o":
+    if green[2][3] == "w" and red[2][1] == "o":
         #turn L once, Ui once
         return true
     if green[2][3] == "o" and red[2][1] == "w":
@@ -127,5 +118,112 @@ def orangepiece( blue[3][3], orange[3][3], green[3][3], red[3][3], yellow[3][3],
             if red[1][2] == "b":
                 #turn Li twice, D twice, R twice
                 return true
-        
 
+def greenpiece( blue[3][3], orange[3][3], green[3][3], red[3][3], yellow[3][3], white[3][3] )
+    #if piece is in the bottom layer
+     for i in 3:
+        if green[3][i] == "g" and yellow[1][i] == "w":
+            #turn B twice
+            return true
+        if orange[3][i] == "g" and yellow[1][i] == "w":
+            #turn D once, B twice
+            return true
+        if blue[3][i] == "g" and yellow[i][1] == "w":
+            #turn D twice, turn B twice
+            return true
+        if red[3][i] == "g" and yellow[i][3]:`
+            #turn Di once, turn R twice
+            return true
+        
+    #second layer
+    if blue[2][1] == "g" and red[2][3] == "w":
+        #Fi once, Di twice, F once, B twice
+        return true
+    if blue[2][1] == "w" and red[2][3] == "g":
+        #turn L once, Di once, Li once, B twice
+        return true
+    if blue[2][3] == "g" and orange[2][1] == "w":
+        #turn F twice, D twice, Fi once, B twice
+        return true
+    if blue[2][3] == "w" and orange[2][1] == "g":
+        #turn Ri once, D once, R once, B twice
+        return true
+    if green[2][1] == "g" and orange[2][3] == "w":
+        #turn Bi once
+        return true
+    if green[2][1] == "w" and orange[2][3] == "g":
+        #turn R once, D once, Ri once, B twice
+        return true
+    if green[2][3] == "w" and red[2][1] == "g":
+        #turn L once, Di once, Li once, B twice
+        return true
+    if green[2][3] == "g" and red[2][1] == "w":
+        #turn B once
+        return true
+    
+    #if piece is in top layer
+    for i in 3 and j in 3:
+        if white[i][j] == "w":
+            if green[1][2] == "g":
+                return true
+            if red[1][2] == "g":
+                #turn Li twice, Di twice, B twice
+                return true
+def redpiece( blue[3][3], orange[3][3], green[3][3], red[3][3], yellow[3][3], white[3][3] )
+    #if piece is in bottom layer
+    for i in 3:
+        if orange[3][i] == "o"and yellow[1][i] == "w":
+            #turn L twice
+            return true
+        if blue[3][i] == "o":
+            for j in 3:
+                if yellow[j][1] == "w":
+                    #turn D once, R twice
+                    return true
+        if green[3][i] == "o":
+            for j in 3:
+                    #turn B once, turn Ri once
+                    return true
+        if red[3][i] == "o" and yellow[3][i] == "w":`
+            #turn Di once, turn R twice
+    
+    #if piece is on the second layer
+    if blue[2][1] == "o" and red[2][3] == "w":
+        #turn F once, D once, Fi once, R three times
+        return true
+    if blue[2][1] == "w" and red[2][3] == "o":
+        #turn R once
+        return true
+    
+    if blue[2][3] == "o" and orange[2][1] == "w":
+        #turn L once, D twice, R twice
+        return true
+    if blue[2][3] == "w" and orange[2][1] == "o":
+        #turn Li twice, Di twice, R once
+        return true
+    
+    if green[2][1] == "o" and orange[2][3] == "w":
+        #turn B once, U twice
+        return true
+    if green[2][1] == "w" and orange[2][3] == "o":
+        #turn Ri once, U once
+        return true
+    
+    if green[2][3] == "w" and red[2][1] == "o":
+        #turn L once, Ui once
+        return true
+    if green[2][3] == "o" and red[2][1] == "w":
+        #turn Bi once, U twice
+        return true
+                
+    #if piece is in top layer
+    for i in 3 and j in 3:
+        if white[i][j] == "w":
+            if orange[1][2] == "o":
+                return true
+            if green[1][2] == "o":
+                #turn B twice, Di once, R twice
+                return true
+            if red[1][2] == "b":
+                #turn Li twice, D twice, R twice
+                return true
