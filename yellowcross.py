453950f725f1fab0cd47172423d46715c0538f68
ashlarin <33299461+ashlarin@users.noreply.github.com>
2018-03-02 00:26:09 -0800

    Add files via upload

---
+++ yellowcross.py
@@ -0,0 +1,55 @@
+def turnalgo():
+	rotateFront()
+	rotateCCTop()
+	rotateRight()
+	rotateTop()
+	rotateCCRight()
+	rotateCCFront()
+
+def yellowCross():
+	count = 0
+	for i in irange(1, 3):
+		for j in jrange(1, 3):
+			if i = 2 and j = 2
+				continue
+			if top[i][j] != "y"
+				count++
+	#if only the center is yellow
+	if count = 8
+		turnalgo()
+	#finding the side of the fish
+	if top[1][2] == "y" and top[2][1] == "y"
+		turnalgo()
+		turnalgo()
+		return true
+	if top[1][2] == "y" and top[2][3] == "y"
+		rotateTop()
+		turnalgo()
+		turnalgo()
+		return true
+	if top[2][3] == "y" and top[3][2] == "y"
+		rotateTop()
+		rotateTop()
+		turnalgo()
+		turnalgo()
+		return true
+	if top[2][1] == "y" and top[3][2] == "y"
+		rotateCCTop()
+		turnalgo()
+		turnalgo()
+		return true
+	#if there is three yellows in a row 
+	if top[2][1] == "y" and top[2][3] == "y"
+		turnalgo()
+		return true
+	if top[1][2] == "y" and top[3][2] == "y"
+		rotateTop()
+		turnalgo()
+		return true
+yellowCross()
+
+
+
+
+
+
