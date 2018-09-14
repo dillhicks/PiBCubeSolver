2537a161d9aa1b7fe3e38925948eee956cafbaae
dillhicks <33649660+dillhicks@users.noreply.github.com>
2018-09-14 13:48:28 -0700

    oops forgot to update

--- main/main.py
+++ main/main.py
@@ -3,12 +3,36 @@ import movement as mv
 import scan 
 import kociemba 
 
-if __name__ == "__main__":
-	face1, face2 = scan.init_scan()
-	mv.init_turn()
-	face3, face4 = scan.init_scan()
-	mv.init_turn()
-	face5, face6 = scan.init_scan()
-	mv.init_turn()
+cube = []
 
-	
+def get_Orientation(face):
+	if face[1][1] == 'W':
+        cube[0] = face;
+    else if face[1][1] == 'G':
+        cube[1] = face;
+    else if face[1][1] == 'O':
+        cube[2] = face;
+    else if face[1][1] == 'B':
+        cube[3] = face;
+    else if face[1][1] == 'R':
+        cube[4] = face;
+    else if face[1][1] == 'Y':
+        cube[5] = face;
+
+if __name__ == "__main__":
+	f1, f2 = scan.init_scan()
+	mv.init_turn()
+	f3, f4 = scan.init_scan()
+	mv.init_turn()
+	f5, f6 = scan.init_scan()
+	mv.init_turn()
+	get_Orientation(f1)
+	get_Orientation(f2)
+	get_Orientation(f3)
+	get_Orientation(f4)
+	get_Orientation(f5)
+	get_Orientation(f6)
+
+
+
+
--- main/scan.py
+++ main/scan.py
@@ -1,13 +1,15 @@
 import cv2
-import numpy
+import numpy as np 
 import maplotlib.pyplot as plt 
 import matplotlib.image as npimg
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 
-CUBE_COLORS = {"Red":(255,0,0), "Yellow":(237,167,16), "Green":(13,186,47), "White":(255,255,255), "Orange":(255,144,0), "Blue":(0,0,255)} 
-CUBE_PIXELS = [ [,,,],  #left(side) top (row) left (column)
+
+#TODO update colors and pixel files 
+CUBE_COLORS = {"R":(255,0,0), "Y":(237,167,16), "G":(13,186,47), "W":(255,255,255), "O":(255,144,0), "B":(0,0,255)} 
+CUBE_PIXELS_L = [[,,,], #left(side) top (row) left (column)
 				[,,,],  #left top center
 				[,,,],  #left top right
 				[,,,],  #left center left
@@ -15,8 +17,9 @@ CUBE_PIXELS = [ [,,,],  #left(side) top (row) left (column)
 				[,,,],  #left center right
 				[,,,],  #left bottom left
 				[,,,],  #left bottom center
-				[,,,],  #left bottom right
-				[,,,],  #right top left
+				[,,,] ] #left bottom right
+
+CUBE_PIXELS_R = [[,,,], #right top left
 				[,,,],  #right top center
 				[,,,],  #right top right
 				[,,,],  #right center left
@@ -34,7 +37,6 @@ def take_picture():
 	img = rawCapture.array
 	return img
 
-
 def color_diff(color1, color2):
 	return sum([abs(component1-component2) for component1, component2 in zip(color1, color2)])
 
@@ -43,19 +45,32 @@ def class_color(color):
 	differences.sort() 
 	color_name = differences[0][1]
 	return color_name
+
 def get_color(img, pixel):
+	pixels = img[pixel[0]:pixel[1], pixel[2]:pixel[3]]
+	acpr = np.average(pixels, axis = 0)
+	ac = np.average(acpr, axis = 0)
+	color_name = class_color(ac)
+	return color_name
 
 def init_scan():
-	take_picture()
-
-
-
-
-
-
-
-
-
-
-
+	img = take_picture() 
+	f1 = np.zeros(shape = (3,3)) #left face
+	f2 = np.zeros(shape = (3,3)) #right face
+	x,y = 0 
+	for pixels in CUBE_PIXELS_L:
+		color = get_color(img, pixels)
+		if x == 3:
+			x = 0
+			y++  
+		f1[x][y] = color
+		x++
+	x,y = 0
+	for pixels in CUBE_PIXELS_R:
+		color = get_color(img,pixels)
+		if x = 3:
+			x = 0
+			y++  
+		f1[x][y] = color
+		x++
 	return f1, f2
